764dfb7cea72313953b2da1a5b5fe66caee39177
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-25 22:56:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" class="w-12 h-12 bg-slate-900 m-5">
+      <img src="/vite.svg" class="w-48 h-48 m-5 bg-orange-700" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
