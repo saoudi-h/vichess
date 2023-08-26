fc53f7476085910aef4537126c158fc2bb38185d
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-26 17:10:42 +0200

    up installation config with new formatting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,8 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++"
-    class="p-14 m-14">count is {{ count }}</button>
+    <button type="button" @click="count++" class="p-14 m-14">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -20,9 +19,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+    official Vue + Vite starter
   </p>
   <p>
     Install
@@ -31,4 +29,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
