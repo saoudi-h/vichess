02c21126659ac4b9130a453926fc7e53c0d7b4e8
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-16 17:00:26 +0200

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log(import.meta.env.VITE_SOME_KEY)
+// console.log(process.env.VUE_APP_NOT_SECRET_CODE)
 createApp(App).mount('#app')
